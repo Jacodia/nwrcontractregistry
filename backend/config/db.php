d9103fea8bcf68a14cd00e1bb93691a69218dea0
urania6 <uraniathomas23@gmail.com>
2025-09-13 11:49:55 +0200

    updated
    login.php, upload files,  updated the dashboard, update the login form

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,13 +1,13 @@
 <?php
 
-$host = "locslhost:8080";
+$host = "localhost";
+$port = "8080"; 
 $user = "root";
 $pass = "";
 $db = "nwr_crdb";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("DB Connection failed: " . $e->getMessage());
